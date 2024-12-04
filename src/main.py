81aa4bd7db0361bd3d49c29f97f7c3a4b245a96e
Jaroslav Hůna <jaroslav@huna.dev>
2024-12-04 21:10:50 +0100

    copy static to public

--- src/main.py
+++ src/main.py
@@ -1,9 +1,25 @@
-from textnode import TextNode
+import os
+import shutil
 
 
 def main():
-    TN = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(TN)
+    copy_src_to_dst("static", "public", False)
+
+
+def copy_src_to_dst(src: str, dst: str, clean: bool):
+    if not os.path.exists(dst):
+        os.mkdir(dst, mode=0o777)
+    elif not clean:
+        shutil.rmtree(dst)
+        os.mkdir(dst, mode=0o777)
+
+    dir = os.listdir(src)
+    for item in dir:
+        if os.path.isdir(os.path.join(src, item)):
+            os.mkdir(os.path.join(dst, item))
+            copy_src_to_dst(os.path.join(src, item), os.path.join(dst, item), True)
+        else:
+            shutil.copy(os.path.join(src, item), dst)
 
 
 if __name__ == "__main__":
